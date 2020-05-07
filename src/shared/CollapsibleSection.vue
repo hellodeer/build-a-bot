78f79b3529cf00c2bbd455d3afd8ed5447489c97
Per A <per.axelsson@wipcore.se>
2020-05-07 15:50:53 +0200

    Injecting Content into Child Component with Slots

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="header">
+            <span v-if="open" @click="open = !open" > &#x25B2;
+                Collapse</span>
+            <span v-if="!open" @click="open = !open" > &#x25BC;
+                Expand</span>
+        </div>
+        <slot v-if="open">
+            <div>This default content will show if no other content was loaded/present</div>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return { open: true };
+  },
+};
+</script>
+
+<style scoped>
+    .header {
+        background-color: grey;
+        padding: 3px;
+        cursor: pointer;
+    }
+</style>
