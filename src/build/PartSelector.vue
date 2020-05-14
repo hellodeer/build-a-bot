b9fc7455ce92603f00703d06c7000ed598afd702
Per A <per.axelsson@wipcore.se>
2020-05-14 15:15:37 +0200

    Creating a custom directive

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -11,12 +11,12 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
 <script>
-
+import pinDirective from '../shared/pin-directive';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -29,6 +29,7 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  directives: { pin: pinDirective },
   props: {
     parts: {
       type: Array,
@@ -57,17 +58,6 @@ export default {
     this.emitSelectedPart();
   },
   methods: {
-    // Instead of wrapping img with <router-link> this was used in previous example
-    // navigate with code : <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
-    // showPartInfo() {
-    //   this.$router.push({
-    //     name: 'Parts',
-    //     params: {
-    //       id: this.selectedPart.id,
-    //       partType: this.selectedPart.type,
-    //     },
-    //   });
-    // },
     emitSelectedPart() {
       this.$emit('partSelected', this.selectedPart);
     },
@@ -97,9 +87,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
