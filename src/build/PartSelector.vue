8e0c334ebbb01f27d290a8d9430d0ffc5d685cc8
Per A <per.axelsson@wipcore.se>
2020-05-14 16:09:17 +0200

    Pass data to directive via object

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -11,7 +11,10 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <!-- Pass argments to custom directive like so -->
+    <!-- <span v-pin:position.bottom.left class="sale" v-show="selectedPart.onSale">Sale!</span> -->
+    <span v-pin= "{bottom: '5px', right: '5px'}" class="sale"
+      v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,8 +1,15 @@
 /* eslint-disable no-param-reassign */
 export default {
-  bind: (element) => {
+  bind: (element, binding) => {
+    // This was used when passing arguments
+    // if (binding.arg !== 'position') return;
+
+    // Object.keys(binding.modifiers).forEach((key) => {
+    //   element.style[key] = '5px';
+    // });
+    Object.keys(binding.value).forEach((position) => {
+      element.style[position] = binding.value[position];
+    });
     element.style.position = 'absolute';
-    element.style.bottom = '5px';
-    element.style.right = '5px';
   },
 };
