b9050d984d9a0bcf7f367edeba9e174bbd97e3b5
Per A <per.axelsson@wipcore.se>
2020-05-15 11:18:44 +0200

    Making directives available globally

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -5,23 +5,3 @@ export default function (element, binding) {
   });
   element.style.position = 'absolute';
 }
-
-
-// There is an easier way to use same functionality for bind and update lifecylce hooks than below.
-// Presented above.
-
-// function applyStyle(element, binding) {
-//   Object.keys(binding.value).forEach((position) => {
-//     element.style[position] = binding.value[position];
-//   });
-//   element.style.position = 'absolute';
-// }
-
-// export default {
-//   bind: (element, binding) => {
-//     applyStyle(element, binding);
-//   },
-//   update: (element, binding) => {
-//     applyStyle(element, binding);
-//   },
-// };
